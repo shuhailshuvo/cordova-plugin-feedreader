9e368152378245e903f24be232538b239a0d5f42
Shuhail <shuhail.alam@gmail.com>
2017-10-31 05:11:31 +0600

    Update FeedReader.js

--- www/FeedReader.js
+++ www/FeedReader.js
@@ -1,15 +1,54 @@
 
 var exec = require('cordova/exec');
 
-var PLUGIN_NAME = 'MyCordovaPlugin';
+var PLUGIN_NAME = 'FeedReader';
 
-var MyCordovaPlugin = {
+var FeedReader = {
   echo: function(phrase, cb) {
     exec(cb, null, PLUGIN_NAME, 'echo', [phrase]);
   },
   getDate: function(cb) {
     exec(cb, null, PLUGIN_NAME, 'getDate', []);
-  }
+  },
+  read: function(url, successFunction, errorFunction)
+		{
+			options = jQuery.extend({
+				url: null,
+				data: null,
+				cache: true,
+				success: null,
+				failure: null,
+				error: null,
+				global: true
+			}, options);
+
+			if (options.url) {
+				if (jQuery.isFunction(options.failure) && jQuery.type(options.error)==='null') {
+				  // Handle legacy failure option
+				  options.error = function(xhr, msg, e){
+					options.failure(msg, e);
+				  }
+				} else if (jQuery.type(options.failure) === jQuery.type(options.error) === 'null') {
+				  // Default error behavior if failure & error both unspecified
+				  options.error = function(xhr, msg, e){
+					window.console&&console.log('getFeed failed to load feed', xhr, msg, e);
+				  }
+				}
+				return $.ajax({
+					type: 'GET',
+					url: options.url,
+					data: options.data,
+					cache: options.cache,
+					dataType: (jQuery.browser.msie) ? "text" : "xml",
+					success: function(xml) {
+						var feed = new JFeed(xml);
+						if (jQuery.isFunction(options.success)) options.success(feed);
+					},
+					error: options.error,
+					global: options.global
+				});
+			}
+		}
 };
 
-module.exports = MyCordovaPlugin;
+module.exports = FeedReader;
