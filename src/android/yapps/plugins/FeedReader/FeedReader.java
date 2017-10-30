0a4497ad05695fbd69a47e9cfe625451a5cc0634
Shuhail <shuhail.alam@gmail.com>
2017-10-31 03:24:58 +0600

    Java

--- src/android/yapps/plugins/FeedReader/FeedReader.java
+++ src/android/yapps/plugins/FeedReader/FeedReader.java
@@ -5,9 +5,14 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 public class FeedReader extends CordovaPlugin {
-
+    private static final String TAG = "FeedReader";
+    
+    public void initialize(CordovaInterface cordova, CordovaWebView webView){
+        super.initialize(cordova,webView);
+        Lod.d(TAG,"Initializing FeedReader");
+    }
     @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("reed")) {
 
