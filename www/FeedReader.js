daad88e4c569e6bd43112bbb3cb389679902e4b1
Shuhail <shuhail.alam@gmail.com>
2017-10-31 03:18:19 +0600

    Preparing Javascript

--- www/FeedReader.js
+++ www/FeedReader.js
@@ -1,48 +1,45 @@
-(function(window, undefined)
-{
-	'use strict';
-	var FeedReader =
-	{
-		read: function(url, successFunction, errorFunction)
-		{
-			options = jQuery.extend({
-				url: null,
-				data: null,
-				cache: true,
-				success: null,
-				failure: null,
-				error: null,
-				global: true
-			}, options);
+var exec = require('cordova/exec');
+var PLUGIN_NAME = "FeedReader";
+var FeedReader = {
+read: function(url, successFunction, errorFunction){
+	options = jQuery.extend({
+		url: null,
+		data: null,
+		cache: true,
+		success: null,
+		failure: null,
+		error: null,
+		global: true
+	}, options);
 
-			if (options.url) {
-				if (jQuery.isFunction(options.failure) && jQuery.type(options.error)==='null') {
-				  // Handle legacy failure option
-				  options.error = function(xhr, msg, e){
-					options.failure(msg, e);
-				  }
-				} else if (jQuery.type(options.failure) === jQuery.type(options.error) === 'null') {
-				  // Default error behavior if failure & error both unspecified
-				  options.error = function(xhr, msg, e){
-					window.console&&console.log('getFeed failed to load feed', xhr, msg, e);
-				  }
-				}
-				return $.ajax({
-					type: 'GET',
-					url: options.url,
-					data: options.data,
-					cache: options.cache,
-					dataType: (jQuery.browser.msie) ? "text" : "xml",
-					success: function(xml) {
-						var feed = new JFeed(xml);
-						if (jQuery.isFunction(options.success)) options.success(feed);
-					},
-					error: options.error,
-					global: options.global
-				});
-			}
+	if (options.url) {
+		if (jQuery.isFunction(options.failure) && jQuery.type(options.error)==='null') {
+		  // Handle legacy failure option
+		  options.error = function(xhr, msg, e){
+			options.failure(msg, e);
+		  }
+		} else if (jQuery.type(options.failure) === jQuery.type(options.error) === 'null') {
+		  // Default error behavior if failure & error both unspecified
+		  options.error = function(xhr, msg, e){
+			window.console&&console.log('getFeed failed to load feed', xhr, msg, e);
+		  }
 		}
-	};
+		return $.ajax({
+			type: 'GET',
+			url: options.url,
+			data: options.data,
+			cache: options.cache,
+			dataType: (jQuery.browser.msie) ? "text" : "xml",
+			success: function(xml) {
+				var feed = new JFeed(xml);
+				if (jQuery.isFunction(options.success)) options.success(feed);
+			},
+			error: options.error,
+			global: options.global
+		});
+	}
+}
+};
 	
 	cordova.addConstructor(function() 
 	{
@@ -170,7 +167,4 @@ JFeed.prototype = {
 			});
 		}
 	};
-
-
-	
-})(window);
+module.exports = FeedReader;
